f9df4e378f97a9af04ec14321a70758fe26726dd
Javi Manzano <javi.manzano.oller@gmail.com>
2015-07-24 23:09:20 +0100

    Static files and grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'static/css/wedding.css' : 'static/scss/wedding.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'static/scss/**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['sass', 'watch']);
+};
