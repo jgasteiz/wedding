489850bb55dee7df1428a490a523251a87affc1c
Javi Manzano <javi.manzano.oller@gmail.com>
2015-07-25 22:27:38 +0100

    Add cms logic for approving and editing submitted songs

--- cms/urls.py
+++ cms/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^songs/$', views.songs, name='songs'),
+    url(r'^songs/approve/$', views.approve_song, name='approve_song'),
+    url(r'^songs/update/(?P<pk>\d+)/$', views.update_song, name='update_song'),
+    url(r'^songs/delete/(?P<pk>\d+)/$', views.delete_song, name='delete_song'),
     url(r'^rsvp/$', views.rsvp, name='rsvp'),
 ]
 
--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,15 @@
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import redirect
+from django.views.generic import (
+    TemplateView,
+    UpdateView,
+    DeleteView,
+    View,
+    ListView,
+)
 
 from wedding import mixins
+from wedding.models import Song
 
 
 class HomeView(mixins.ViewNameMixin, TemplateView):
@@ -10,11 +19,47 @@ class HomeView(mixins.ViewNameMixin, TemplateView):
 home = HomeView.as_view()
 
 
-class SongsView(mixins.ViewNameMixin, TemplateView):
+class SongsView(mixins.ViewNameMixin, ListView):
     page_name = 'songs'
     template_name = 'cms/songs.html'
+    model = Song
 
 songs = SongsView.as_view()
+
+
+class UpdateSongView(mixins.ViewNameMixin, UpdateView):
+    page_name = 'songs'
+    template_name = 'cms/song_form.html'
+    model = Song
+    success_url = reverse_lazy('cms:songs')
+
+update_song = UpdateSongView.as_view()
+
+
+class DeleteSongView(mixins.ViewNameMixin, DeleteView):
+    page_name = 'songs'
+    template_name = 'cms/song_delete_confirmation.html'
+    model = Song
+    success_url = reverse_lazy('cms:songs')
+
+delete_song = DeleteSongView.as_view()
+
+
+class ApproveSong(View):
+    success_url = reverse_lazy('cms:songs')
+
+    def post(self, request, *args, **kwargs):
+        song_id = int(request.POST.get('song_id'))
+        song_approved = request.POST.get('is_approved') == 'True'
+        song = Song.objects.get(id=song_id)
+        song.is_approved = song_approved
+        song.save()
+        return redirect(self.success_url)
+
+    def get(self, request, *args, **kwargs):
+        return redirect(self.success_url)
+
+approve_song = ApproveSong.as_view()
 
 
 class RsvpView(mixins.ViewNameMixin, TemplateView):
