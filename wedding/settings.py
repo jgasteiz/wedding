9b00f98958137e642029c97fbdc396e403115cc3
Javi Manzano <javi.manzano.oller@gmail.com>
2015-07-25 22:24:50 +0100

    Add settings for chaching and logging

--- wedding/settings.py
+++ wedding/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = (
     'djangae.contrib.security',
     'djangae.contrib.uniquetool',
 
+    'wedding',
     'public',
     'cms',
 )
@@ -121,6 +122,39 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'djangae': {
+            'level': 'WARN',
+        }
+    }
+}
 
 
 if DEBUG:
