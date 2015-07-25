0212310ac96c81873a847f07b11463a515c4611c
Javi Manzano <javi.manzano.oller@gmail.com>
2015-07-25 23:09:08 +0100

    Change labels of song in form and cms

--- wedding/forms.py
+++ wedding/forms.py
@@ -4,6 +4,9 @@ from .models import Song
 
 
 class SongForm(forms.ModelForm):
+    name = forms.CharField(label='Song title')
+    artist = forms.CharField(label='Artist name')
+
     class Meta:
         fields = ('name', 'artist',)
         model = Song
