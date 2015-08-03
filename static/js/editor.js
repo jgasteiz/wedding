7f215e15fffe2769f32dbd885178ce48d285ff0d
Javi Manzano <javi.manzano.oller@gmail.com>
2015-08-03 23:01:08 +0100

    Add html preview next to the editor

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,12 +1,18 @@
 (function () {
     'use_strict';
 
-    var editor = ace.edit("html");
-    var textarea = $('textarea[name="html"]');
+    var editor = ace.edit('html'),
+        textarea = $('textarea[name="html"]'),
+        $iframe = $('#html-preview');
+
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/html");
     editor.getSession().setValue(textarea.val());
     editor.getSession().on('change', function(){
-      textarea.val(editor.getSession().getValue());
+        var html = editor.getSession().getValue();
+        textarea.val(html);
+        $iframe.contents().find("body").html(html);
     });
+
+    $iframe.contents().find("body").html(textarea.val());
 })();
