91c9d170cc4e4a438949a70a368ab8268fbba972
Javi Manzano <javi.manzano.oller@gmail.com>
2015-08-06 21:01:41 +0100

    Get rid of InvitationEmail model and add a dynamic model

--- wedding/models.py
+++ wedding/models.py
@@ -36,9 +36,40 @@ class Invitee(models.Model):
         return '{} {}'.format(self.first_name, self.last_name)
 
 
-class InvitationEmail(models.Model):
-    email_language = models.CharField(max_length=256)
-    html = models.TextField()
+def create_model(name, fields=None, app_label='', module='', options=None):
+    """
+    Create specified model
+    """
+    class Meta:
+        # Using type('Meta', ...) gives a dictproxy error during model creation
+        pass
 
-    def __unicode__(self):
-        return 'Invitation email in {} language'.format(self.email_language)
+    if app_label:
+        # app_label must be set using the Meta inner class
+        setattr(Meta, 'app_label', app_label)
+
+    # Update Meta with any options that were provided
+    if options is not None:
+        for key, value in options.iteritems():
+            setattr(Meta, key, value)
+
+    # Set up a dictionary to simulate declarations within a class
+    attrs = {'__module__': module, 'Meta': Meta}
+
+    # Add in any fields that were provided
+    if fields:
+        attrs.update(fields)
+
+    # Create the class, which automatically triggers ModelBase processing
+    model = type(name, (models.Model,), attrs)
+
+    return model
+
+
+def get_email_class():
+    fields = {
+        'name': models.CharField(max_length=256)
+    }
+    for language_key, language_name in LANGUAGES:
+        fields['html_{}'.format(language_key)] = models.TextField(blank=True, null=True)
+    return create_model('Email', fields, 'wedding')
