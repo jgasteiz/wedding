4f5495aac2a20f5d3458f09202a1a335b8f79546
Javi Manzano <javi.manzano.oller@gmail.com>
2015-08-06 21:03:07 +0100

    Adjust EmailForm to the new dynamic model

--- wedding/forms.py
+++ wedding/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from django.db import models
 
 from .constants import LANGUAGES, INVITATION_STATUSES, INVITER_CHOICES
+from django.forms import model_to_dict
+from .models import Invitee, Song, get_email_class
 
 
 class InviteeForm(forms.ModelForm):
@@ -33,3 +36,26 @@ class SongForm(forms.ModelForm):
 class EmailForm(forms.Form):
     name = forms.CharField()
 
+    def __init__(self, instance=None, *args, **kwargs):
+        super(EmailForm, self).__init__(*args, **kwargs)
+
+        if instance is None:
+            Email = get_email_class()
+            self.instance = Email()
+        else:
+            self.instance = instance
+
+        self.initial = model_to_dict(self.instance)
+
+        for language_key, _ in LANGUAGES:
+            field_name = 'html_{}'.format(language_key)
+            self.fields[field_name] = forms.CharField(widget=forms.Textarea, required=False)
+            self.fields[field_name].is_html = True
+
+    def save(self, *args, **kwargs):
+        cleaned_data = self.cleaned_data
+        for field_name in cleaned_data.keys():
+            setattr(self.instance, field_name, cleaned_data[field_name])
+
+        self.instance.save()
+        return self.instance
