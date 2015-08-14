ff0e4ff3ec5d4a51557686daad94e6fc506f8033
Javi Manzano <javi.manzano.oller@gmail.com>
2015-08-14 15:21:23 +0100

    Add tests for send_email view

--- cms/tests/test_views.py
+++ cms/tests/test_views.py
@@ -1,8 +1,93 @@
 # coding: utf-8
 from django.core.urlresolvers import reverse
 
-from wedding.models import get_email_class
+from wedding.models import get_email_class, Invitee
 from wedding.tests import AppEngineTestCase
+
+
+class SendEmailsViewTestCase(AppEngineTestCase):
+    def setUp(self):
+        super(SendEmailsViewTestCase, self).setUp()
+        email_class = get_email_class()
+
+        # Create some emails.
+        self.email1 = email_class.objects.create(
+            name=u'Test email 1',
+            html_en=u'<h1>Test en</h1>',
+            html_es=u'<h1>Test es</h1>',
+            html_pl=u'<h1>Test pl</h1>',
+        )
+        self.email2 = email_class.objects.create(
+            name=u'Test email 2',
+            html_en=u'<h1>Test en</h1>',
+            html_es=u'<h1>Test es</h1>',
+            html_pl=u'<h1>Test pl</h1>',
+        )
+
+        # Create a few invitees
+        self.invitee1 = Invitee.objects.create(
+            first_name='Jon',
+            last_name='Snow',
+            email='jonsnow@thewall.com',
+            language='en',
+        )
+        self.invitee2 = Invitee.objects.create(
+            first_name='Daenerys',
+            last_name='Targaryen',
+            email='khaleesi@meereen.com',
+            language='en',
+        )
+
+    def test_send_email_to_invitees(self):
+
+        # Test sending the email 1 to both invitees
+        payload = {
+            'email': self.email1.pk,
+            'invitees[]': [self.invitee1.pk, self.invitee2.pk],
+        }
+        response = self.client.post(reverse('cms:send_emails'), payload)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, 'OK')
+
+        # Make sure both invitees got the email
+        self.invitee1 = Invitee.objects.get(pk=self.invitee1.pk)
+        self.assertEqual(self.invitee1.emails, str(self.email1.pk))
+        self.invitee2 = Invitee.objects.get(pk=self.invitee2.pk)
+        self.assertEqual(self.invitee2.emails, str(self.email1.pk))
+
+    def test_not_send_same_email_twice(self):
+
+        # Let's say invitee1 got email1 already.
+        self.invitee1.emails = str(self.email1.pk)
+
+        # Test sending the email 1 to both invitees
+        payload = {
+            'email': self.email1.pk,
+            'invitees[]': [self.invitee1.pk],
+        }
+        response = self.client.post(reverse('cms:send_emails'), payload)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, 'OK')
+
+        # Make sure nothing really changed
+        self.invitee1 = Invitee.objects.get(pk=self.invitee1.pk)
+        self.assertEqual(self.invitee1.emails, str(self.email1.pk))
+
+        # Send email 2 now.
+        payload = {
+            'email': self.email2.pk,
+            'invitees[]': [self.invitee1.pk],
+        }
+        response = self.client.post(reverse('cms:send_emails'), payload)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, 'OK')
+
+        # Make sure invitee got email 2.
+        self.invitee1 = Invitee.objects.get(pk=self.invitee1.pk)
+        self.assertEqual(self.invitee1.emails, ','.join([str(self.email1.pk), str(self.email2.pk)]))
 
 
 class EmailViewsTestCase(AppEngineTestCase):
