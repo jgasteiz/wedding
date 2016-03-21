c779f0d270b96c1472e62dfc13a121d18d7dcf12
Javi Manzano <javi.manzano.oller@gmail.com>
2016-03-21 16:04:54 +0100

    More content updates

--- translate.sh
+++ translate.sh
@@ -1,1 +1,1 @@
-./manage.py makemessages -l pl -l es -i "google_appengine*" -i "django*" -i "djangae*" && ./manage.py compilemessages
+./manage.py makemessages -l pl -l es -i "google_appengine*" -i "django*" -i "djangae*" -i "node_modules*" -i "env*" && ./manage.py compilemessages
